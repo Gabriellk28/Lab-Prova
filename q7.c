dd096b69599cd1b83d8e77bf5cd7504d77aa1599
uyanê <uyanelol2016@gmail.com>
2024-03-02 19:11:09 -0300

    Questão 7

--- q7.c
+++ q7.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
 
-int isPrime(int n, int i){
-
+int isPrime(int n, int divisor){
+  if (n <= 1){
+    return 0; 
+  } 
+  if (divisor == 1){
+    return 1; 
+  }
+  if (n % divisor == 0){
+    return 0; 
+  }
+    return isPrime(n, divisor - 1);
 }
 
-int main(void)
+int main()
 {
+  int num, retorno;
+  printf("Digite um número inteiro positivo: ");
+  scanf("%d", &num);
 
+  retorno = isPrime(num, num/2);
+
+  if(retorno == 0){
+    printf("%d não é primo\n", num);
+  }
+
+  if(retorno == 1){
+    printf("%d é primo\n", num);
+  }
   return 0;
 }
